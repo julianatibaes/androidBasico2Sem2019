77d820c83f52c0ebfa051646a87f10db63ea3df4
juliana <tibaes.juliana@gmail.com>
2019-10-02 20:34:11 -0300

    fornecedores parte 4

---
+++ Fornecedores-parte4/app/src/main/java/com/tibaes/fornecedores/Fornecedor.kt
@@ -0,0 +1,12 @@
+package com.tibaes.fornecedores
+
+class Fornecedor (
+    var nome: String = "",
+    var email: String = "",
+    var telefone: String = "",
+    var classificacao: Float = 2F
+)
+
+    
+
+
